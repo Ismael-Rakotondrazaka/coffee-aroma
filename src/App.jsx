c5e9273f4b174580b47bcc804a64916e99fea939
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-02-10 07:55:41 +0300

    Feat: Create TheFooter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import TheFooter from "./components/TheFooter";
 import TheHeader from "./components/TheHeader";
 
 function App() {
   return (
     <>
       <TheHeader />
+
+      <div className="min-h-screen"></div>
+
+      <TheFooter />
     </>
   );
 }
