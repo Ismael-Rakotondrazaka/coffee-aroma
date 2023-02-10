d31f46e3ffc1a77d699446c137e27d238865c7cc
Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-02-10 08:58:03 +0300

    Feat: Create SectionTitle component

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,9 @@
+function SectionTitle(props) {
+  return (
+    <h1 className="mb-10 text-4xl font-bold text-center text-white uppercase">
+      {props.children}
+    </h1>
+  );
+}
+
+export default SectionTitle;
---
+++ src/components/TheService.jsx
@@ -0,0 +1,15 @@
+import SectionTitle from "./SectionTitle.jsx";
+
+function TheService(props) {
+  return (
+    <div className="min-h-screen px-3 py-20">
+      <SectionTitle>Our service</SectionTitle>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, omnis.
+      </p>
+    </div>
+  );
+}
+
+export default TheService;
